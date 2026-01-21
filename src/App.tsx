4cb0bb6bc6d5405c4bcb1b4b17cdff0cd5ebd941
ics-oka <oka@ics-web.jp>
2026-01-21 17:21:40 +0900

    formatに差分が出るキーを変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import { useState } from "react";
-import { useEffect } from "react";  // biome checkによってuseStateとまとめられる
+import { useEffect } from "react"; // biome checkによってuseStateとまとめられる
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
   const message = "Click Me!";
 
-	const unusedObject = {
-		"keyA": "valueA",
-		"π": 3.14,  // Prettierではダブルクウォートが外れないが、Biomeでは外れる
-	};
+  const unusedObject = {
+    "A": "アルファベットのA",
+    "𐊠": "カリア文字のA", // Prettierではダブルクウォートが外れないが、Biomeでは外れる
+  };
 
   return (
-    <>
-      <h1>{message}</h1><button
-        type="button"
-        className="tree"
-        onClick={() => {setCount((prev) => prev + 1);;}}
+    <><h1>{message}</h1><button type="button" className="tree"
+																onClick={() => {setCount((prev) => prev + 1);;}}
       >
         🌲
       </button>
-                    <div className="count">count is {count}</div>
+      								<div className="count">count is {count}</div>
     </>
   );
 }
