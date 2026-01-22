e556c5b5b96ac661328fc5594b48587dd90309b0
ics-oka <oka@ics-web.jp>
2026-01-22 11:55:33 +0900

    横にコードが長かったので縮める

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useEffect } from "react"; // biome checkによってuseStateとまとめられる
+import { useEffect } from "react"; // biome checkでまとめられる
 import "./App.css";
 
 function App() {
@@ -8,16 +8,16 @@ function App() {
 
   const unusedObject = {
     "A": "アルファベットのA",
-    "𐊠": "カリア文字のA", // Prettierではダブルクウォートが外れないが、Biomeでは外れる
+    "𐊠": "カリア文字のA", // Biomeでは引用符が外れる
   };
 
   return (
     <><h1>{message}</h1><button type="button" className="tree"
-																onClick={() => {setCount((prev) => prev + 1);;}}
+                    onClick={() => {setCount((prev) => prev + 1);;}}
       >
         🌲
       </button>
-      								<div className="count">count is {count}</div>
+              <div className="count">count is {count}</div>
     </>
   );
 }
