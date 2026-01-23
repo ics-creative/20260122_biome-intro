d7e82c4565add60b200e95a16bee58a79b3ebc00
ics-oka <oka@ics-web.jp>
2026-01-23 17:45:17 +0900

    コメント削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import { useState } from "react";
-import { useEffect } from "react"; // biome checkでまとめられる
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
   const message = "Click Me!";
-
-  const unusedObject = {
-    "A": "アルファベットのA",
-    "𐊠": "カリア文字のA", // Biomeでは引用符が外れる
-  };
 
   return (
     <><h1>{message}</h1><button type="button" className="tree"
